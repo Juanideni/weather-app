16f806fda79e1a18d80665ad19310791cc8b6cfa
Juan Denipoti <juanidenipoti11@gmail.com>
2022-01-09 16:49:02 -0300

    flex added

--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -1,7 +1,7 @@
+import {Card} from "react-bootstrap"
 import {useState, useEffect} from "react"
 
 var dayjs = require('dayjs')
-
 
 function WeatherInfo(props){
     const [weatherInfo, setWeatherInfo] = useState([])
@@ -24,69 +24,88 @@ function WeatherInfo(props){
             return("")
         }
     })
+
+    
+
     const imgUrlBase = 'https://www.metaweather.com/static/';
   
-
     return(
         <>
+
         
-        <ul>
-            
+             <ul>  
             {
                 weatherInfo.map(clima => (
                     <>
                             <li key="clima.id">
-                           
-                                    
+                                <div className="div-day">
+
                                     {
                                         (dayjs(clima.applicable_date).format("dddd DD/MM/YYYY") === todayDate) ?
                                         <>
-                                        
-                                            
-                                                
-                                                <h1>
-                                                    {props.name}
-                                                    </h1>
-                                                    <h4>
-                                                    {dayjs(clima.applicable_date).format("dddd")}<br></br>
-                                                    {dayjs(clima.applicable_date).format("DD/MM")}
-                                                    </h4>
-                                                    <br></br>
-                                                        
-                                                <img className="mb-2" width="30" src={`${imgUrlBase}img/weather/${clima.weather_state_abbr}.svg`} alt="" /><br></br>
-                                                  
-                                               
-                                                
-                                                <p>{clima.weather_state_name}</p>
-                                                <p>Min temp: {Number(clima.min_temp).toFixed(2)}° </p>
-                                                <p>Max temp: {Number(clima.max_temp).toFixed(2)}°</p>
-                                                <p>Current temp: {Number(clima.the_temp).toFixed(2)}°</p>
-                                                <p>Air pressure: {clima.air_pressure} hPa</p>
-                                                <p>Humidity: {clima.humidity} % </p>
-                                                <hr></hr>
-                                                 
-                                                
-                                        
-                                        </>
-                                        :
-                                        <>
-                                        
-                                        <p>
-                                            
-                                            <img className="mb-2" width="30" src={`${imgUrlBase}img/weather/${clima.weather_state_abbr}.svg`} alt="" /><br></br>
-                                            {dayjs(clima.applicable_date).format("ddd")}
-                                            <p>{Number(clima.min_temp).toFixed(1)} - {Number(clima.max_temp).toFixed(1)}</p>
-                                            </p>
-                                        </>
-                                    }
+                                         <h1>
+                                             <h1>{props.name}</h1>
+                                             <h1>{dayjs(clima.applicable_date).format("dddd")}</h1>
+                                             <h1>{dayjs(clima.applicable_date).format("DD/MM")}</h1>
+                                             <h1>{Number(clima.the_temp).toFixed(2)}°</h1>
+                                             <h1><img className="mb-2" width="30" src={`${imgUrlBase}img/weather/${clima.weather_state_abbr}.svg`} alt="" /></h1>
+                                         <h5>{clima.weather_state_name}</h5>
+                                         </h1>
+                                         <h5>  
+                                                 <h1>Min temp: {Number(clima.min_temp).toFixed(2)}° </h1>
+                                                 <h1>Max temp: {Number(clima.max_temp).toFixed(2)}°</h1>
+                                                 <h1>Air pressure: {clima.air_pressure} mbar</h1>
+                                                 <h1>Humidity: {clima.humidity} % </h1>
+                                                 <h1>Wind: {Number(clima.wind_speed).toFixed(1)}mph at {Number(clima.wind_direction).toFixed(1)}° {clima.wind_direction_compass}</h1>
+                                                 <h1>Visibility: {Number(clima.visibility).toFixed(1)} miles</h1>
+                                                 <hr></hr>
+                                         </h5>
+                                     </>
+                                         :
+                                         ""
+                                        }
+                                    </div>
+ 
                                     
-
-                                        
+                                    
                                 </li>
                                </>
                             ))
                         }
                     </ul>
+                   
+        
+
+
+
+            
+        <ul className="api-ul">  
+            {
+                weatherInfo.map(clima => (
+                    <>
+                            <li key="clima.id">
+                                    {
+                                        <>
+                                        <Card className="div-card">
+                                            <Card.Body>
+                                                <Card.Title>{dayjs(clima.applicable_date).format("ddd")}</Card.Title>
+                                                <Card.Subtitle className="mb-2 text-muted">{clima.weather_state_name}</Card.Subtitle>
+                                                <Card.Text>
+                                                <img className="mb-2" width="30" src={`${imgUrlBase}img/weather/${clima.weather_state_abbr}.svg`} alt="" /> <br></br>
+                                                {Number(clima.min_temp).toFixed(1)}° / {Number(clima.max_temp).toFixed(1)}°
+                                                </Card.Text>
+                                             
+                                            </Card.Body>
+                                        </Card>
+                                    </>
+                                    }
+                                    
+                                </li>
+                               </>
+                            ))
+                        }
+                    </ul>
+                   
                 
     </>
     )
